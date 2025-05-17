635280053a1a4be44db8d4fe49119ee898ef1ef1
Juan Diego <juan.rodriguez16@uceva.edu.co>
2025-05-17 18:07:13 -0500

    Ajuste la clase JavaCostos

--- src/main/java/libcostos/JavaCostos.java
+++ src/main/java/libcostos/JavaCostos.java
@@ -6,7 +6,7 @@ public class JavaCostos {
 
     public JavaCostos() {
         try {
-            System.loadLibrary("costos"); // Asegúrate que la librería se llame libcostos.so / costos.dll / libcostos.dylib
+            System.loadLibrary("costos");
             System.out.println("Biblioteca 'costos' cargada exitosamente!");
         } catch (UnsatisfiedLinkError e) {
             System.err.println("Error al cargar la biblioteca : " + e.getMessage());
